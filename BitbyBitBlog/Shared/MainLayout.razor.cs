f002f9e1d6df543c5423e951205ff1a3a8ff1ba3
Alvaro Montoya <alvaro.montoya@knights.ucf.edu>
2022-01-21 11:10:16 -0500

    starting to add dark mode

---
+++ BitbyBitBlog/Shared/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+
+namespace BitbyBitBlog.Shared
+{
+    public partial class MainLayout
+    {
+        private MudTheme _currentTheme = new MudTheme();
+        private readonly MudTheme _defaultTheme = new MudTheme()
+        {
+            Palette = new Palette()
+            {
+                Primary = Colors.Indigo.Default,
+                Secondary = Colors.Pink.Default,
+                Background = Colors.Grey.Default
+            }
+        };
+    }
+}
